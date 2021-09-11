fbef2b09c4b1ec69cac4afa03b9ce4429dd462e0
prajwalshinde31 <87930467+prajwalshinde31@users.noreply.github.com>
2021-09-11 14:42:33 +0530

    program 1 added

---
+++ program1.py
@@ -0,0 +1,18 @@
+def func1(arr):
+
+    sum = 0
+    product = 1
+
+    for i in arr:
+        sum += i
+
+    for j in arr:
+        product *= i
+
+    print(sum, product)
+
+
+array = [1,2,3]
+func1(array)
+
+
